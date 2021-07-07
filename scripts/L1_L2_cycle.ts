cccb09651d0ab31dfa191e35f833410c5b0feb67
NIC619 <twedusuck@gmail.com>
2021-07-07 12:25:18 +0800

    Add checkPendingWithdrawals before cycle

--- scripts/L1_L2_cycle.ts
+++ scripts/L1_L2_cycle.ts
@@ -1,18 +1,22 @@
+import * as fs from "fs"
+import * as path from "path"
 import { config, ethers } from "hardhat"
 import { loadContract } from "@eth-optimism/contracts"
 import { sleep } from "@eth-optimism/core-utils"
-import { getMessagesAndProofsForL2Transaction } from "@eth-optimism/message-relayer"
+import { getMessagesAndProofsForL2Transaction, getStateBatchAppendedEventByTransactionIndex } from "@eth-optimism/message-relayer"
 import { Watcher } from "@eth-optimism/watcher"
 import { instance } from "./utils"
 
 const conf: any = config.networks.kovan
 
 const BLOCKTIME_SECONDS = conf.blocktime
-const CHALLENGE_PERIOD_SECONDS = 60 * BLOCKTIME_SECONDS // 60 blocks for challenge period in Kovan
+const CHALLENGE_PERIOD_BLOCKS = 60
+const CHALLENGE_PERIOD_SECONDS = CHALLENGE_PERIOD_BLOCKS * BLOCKTIME_SECONDS // 60 blocks for challenge period in Kovan
+const WATCHER_POLL_INTERVAL = 1500 // 1.5s
 
 // Set up our RPC provider connections.
 const l1RpcProviderUrl = (config.networks.kovan as any).url
-const l1RpcProvider = ethers.provider
+const l1RpcProvider = new ethers.providers.JsonRpcProvider(l1RpcProviderUrl)
 const l2RpcProviderUrl = conf.optimismURL
 const l2RpcProvider = new ethers.providers.JsonRpcProvider(l2RpcProviderUrl)
 
@@ -42,13 +46,100 @@ const watcher = new Watcher({
     l2: {
         provider: l2RpcProvider,
         messengerAddress: l2CrossDomainMessengerAddress
-    }
+    },
+    pollInterval: WATCHER_POLL_INTERVAL
 })
 
 const l1ERC20Address = "0x0712629Ced85A3A62E5BCa96303b8fdd06CBF8dd" // Kovan LON
 const l2ERC20Address = "0x235d9B4249E9C9D705fAC6E98F7D21E58091220A"
 const L1_ERC20 = instance("ERC20", l1ERC20Address, l1RpcProvider)
 const L2_ERC20 = instance("L2StandardERC20Initializeable", l2ERC20Address, l2RpcProvider, true)
+
+async function checkPendingWithdrawals() {
+    const pendingWithdrawalsFilePath = path.join(
+        config.paths["root"],
+        "pendingWithdrawals.json"
+    )
+    const pendingWithdrawals = require(pendingWithdrawalsFilePath)
+    for (const [L2_tx_hash, status] of Object.entries(pendingWithdrawals)) {
+        console.log(L2_tx_hash)
+
+        // TODO: how to verify if it is indded a withdraw tx?
+
+        // Verify if L2 withdraw tx succeeded
+        const l2Transaction = await l2RpcProvider.getTransaction(L2_tx_hash)
+        if (l2Transaction === null) {
+            console.log(`Can not find L2 tx: ${L2_tx_hash}`)
+            console.log("Dropping it from pendingWithdrawals file...")
+            delete pendingWithdrawals[L2_tx_hash]
+            continue
+        }
+        const L2_tx_receipt = await l2RpcProvider.getTransactionReceipt(L2_tx_hash)
+        if (L2_tx_receipt == null) {
+            console.log(`Can not find tx receipt for L2 tx: ${L2_tx_hash}`)
+            continue
+        }
+        if (L2_tx_receipt.status == 0) {
+            console.log(`tx reverted for L2 tx: ${L2_tx_hash}`)
+            console.log("Dropping it from pendingWithdrawals file...")
+            delete pendingWithdrawals[L2_tx_hash]
+            continue
+        }
+        // Verify it has been included in tx batch and was sent to L1
+        pendingWithdrawals[L2_tx_hash]["status"] = "Confirmed on L2"
+        const NUM_L2_GENESIS_BLOCKS = 1
+        const stateBatchAppendedEvent = await getStateBatchAppendedEventByTransactionIndex(
+            // @ts-ignore
+            l1RpcProvider,
+            l1StateCommitmentChainAddress,
+            // @ts-ignore
+            l2Transaction.blockNumber - NUM_L2_GENESIS_BLOCKS
+        )
+        if (stateBatchAppendedEvent === null) {
+            console.log(`L2 tx: ${L2_tx_hash} is not batched into L1 yet`)
+            continue
+        }
+
+        const stateBatchTransaction = await stateBatchAppendedEvent.getTransaction()
+        const L1_sate_root_submission_tx_hash = stateBatchTransaction.hash
+        const L1_sate_root_submission_tx_receipt = await l1RpcProvider.getTransactionReceipt(L1_sate_root_submission_tx_hash)
+        const inclusionBlockNumber = L1_sate_root_submission_tx_receipt.blockNumber
+        const latestBlockNumber = await l1RpcProvider.getBlockNumber()
+        // TODO: this check might be redundant
+        if (latestBlockNumber < inclusionBlockNumber) throw Error(
+            "Something went wrong. Latest block number is smaller than L2 tx inclusion block number"
+        )
+        if (latestBlockNumber - inclusionBlockNumber < CHALLENGE_PERIOD_BLOCKS) {
+            console.log(`L2 withdraw tx: ${L2_tx_hash} is still in challenge period`)
+            pendingWithdrawals[L2_tx_hash]["status"] = "Still in challenge period"
+            continue
+        }
+        // Check if it's already relayed or it's not a cross domain tx at all
+        const [L2_tx_msg_hash] = await watcher.getMessageHashesFromL2Tx(L2_tx_hash)
+        if (L2_tx_msg_hash == undefined) {
+            console.log(`Not a cross domain tx: ${L2_tx_hash}`)
+            console.log("Dropping it from pendingWithdrawals file...")
+            delete pendingWithdrawals[L2_tx_hash]
+            continue
+        } else {
+            // Watcher will throw error if same messages are relayed multiple times on L1
+            // But it should not be the case for a withdraw tx
+            // TODO: verify if failed relayed is included in this case
+            const L1_tx_receipt = await watcher.getL1TransactionReceipt(L2_tx_msg_hash, false)
+            if (L1_tx_receipt == undefined) {
+                console.log(`L2 withdraw tx: ${L2_tx_hash} is ready to be relayed`)
+                pendingWithdrawals[L2_tx_hash]["status"] = "Ready to be relayed"
+            } else {
+                // TODO: related to previous TODO, should we check if the L1 withdraw tx succeed?
+                console.log(`L2 withdraw tx: ${L2_tx_hash} is already relayed by L1 tx: ${L1_tx_receipt.transactionHash}`)
+                console.log("Dropping it from pendingWithdrawals file...")
+                delete pendingWithdrawals[L2_tx_hash]    
+            }
+            continue
+        }
+    }
+    fs.writeFileSync(pendingWithdrawalsFilePath, JSON.stringify(pendingWithdrawals, null, 2))
+}
 
 async function cycle() {
     // Initial balances.
@@ -208,18 +299,20 @@ async function cycle() {
 
 async function main() {
     // Checking L2 ERC20
-    const erc20L1TokenStored = await L2_ERC20.callStatic.l1Token()
-    if (erc20L1TokenStored !== L1_ERC20.address) {
-        throw new Error("L1 ERC20 token address was not correctly set")
-    }
-    const erc20L2TokenBridgeStored = await L2_ERC20.callStatic.l2Bridge()
-    if (erc20L2TokenBridgeStored !== L2_StandardBridge.address) {
-        throw new Error("L2 bridge address was not correctly set")
-    }
+    // const erc20L1TokenStored = await L2_ERC20.callStatic.l1Token()
+    // if (erc20L1TokenStored !== L1_ERC20.address) {
+    //     throw new Error("L1 ERC20 token address was not correctly set")
+    // }
+    // const erc20L2TokenBridgeStored = await L2_ERC20.callStatic.l2Bridge()
+    // if (erc20L2TokenBridgeStored !== L2_StandardBridge.address) {
+    //     throw new Error("L2 bridge address was not correctly set")
+    // }
 
-    while (true) {
-        await cycle()
-    }
+    await checkPendingWithdrawals()
+
+    // while (true) {
+    //     await cycle()
+    // }
 }
 
 main()
