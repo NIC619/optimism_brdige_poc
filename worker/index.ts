1d75cd33e3ba15d68ed5073a5801490a4f7e85b1
NIC619 <twedusuck@gmail.com>
2021-07-12 19:40:46 +0800

    Replace watcher with scanner

--- worker/index.ts
+++ worker/index.ts
@@ -1,10 +1,13 @@
 import schedule from "node-schedule"
 import watcher from "./watcher"
+import scanner from "./scanner"
 import worker from "./worker"
 
 async function main() {
-    await watcher()
+    // await watcher()
+    await scanner()
     await worker()
+    schedule.scheduleJob("*/1 * * * *", scanner)
     schedule.scheduleJob("*/3 * * * *", worker)
 }
 
